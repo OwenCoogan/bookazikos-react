f6e214b96870a882192182bebdaf521eceda6776
Owen Coogan <owencoogan01@gmail.com>
2022-10-18 21:41:44 +0200

    added stuff

--- src/components/UI/HeaderHero.tsx
+++ src/components/UI/HeaderHero.tsx
@@ -4,21 +4,21 @@ import ButtonLink from '../design-system/buttons/ButtonLink';
 export default function HeaderHero() {
   return (
     <section>
-	<div className="bg-yellow-300 text-white py-20">
+	<div className="bg-yellow-300 text-white py-20 align-center">
 		<div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
-			<div className="flex flex-col lg:w-1/3 justify-center items-start p-8 w-full">
+			<div className="flex flex-col lg:w-1/3 justify-center items-center lg:items-start text-center lg:text-left p-8 w-full">
 			<h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Nous travaillons pour l’égalité fille garçon</h1>
             <ButtonLink
 							route="/contact"
 							text="Contactez nous"
 						/>
 			</div>
-			<div className="p-8 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
+			<div className="hidden lg:block p-8 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
 				<div className="h-48 flex flex-wrap content-center">
 					<div
 						className='m-auto'
 					>
-						<img className="inline-block hidden xl:block max-w-md" src={holdingHands} alt="Hello"/>
+						<img className="lg:inline-block hidden xl:block max-w-md" src={holdingHands} alt="Hello"/>
 					</div>
 				</div>
 			</div>
